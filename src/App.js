b9d8d4743b7c48c1d3d58a472a8c8d0541fdd984
Olga Runova <ockaaa@gmail.com>
2021-04-03 00:31:51 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <div>
         <Menu />
         <div className="page">
-          <Route path="/" exact component={HomePage} />
+          <Route path="/react-router/" exact component={HomePage} />
           <Route path="/drift" component={DriftPage} />
           <Route path="/timeattack" component={TimeAttackPage} />
           <Route path="/forza" component={ForzaPage} />
